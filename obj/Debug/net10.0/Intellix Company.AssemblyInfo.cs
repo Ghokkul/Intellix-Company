3acf87682e51d5b0910a739ef3c37cf49cf215f1
Ghokkul <botnetworkgaming123@gmail.com>
2025-11-18 20:33:09 +1300

    updates

--- obj/Debug/net10.0/Intellix Company.AssemblyInfo.cs
+++ obj/Debug/net10.0/Intellix Company.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Intellix Company")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c85381090614198326be07e70019e0f16cdc21bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a74d6a1f0c9631688e90d33d44c6ca2695e50672")]
 [assembly: System.Reflection.AssemblyProductAttribute("Intellix Company")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Intellix Company")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
